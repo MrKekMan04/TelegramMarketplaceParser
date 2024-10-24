bc544106d3c1d841e1f7a5338d48b328dcec099a
Vitaliy <vitaliy-efimov111@mail.ru>
2024-10-25 01:11:39 +0500

    Реализовать логику эндпоинтов

--- gateway/src/main/java/ru/overcode/gateway/mapper/chatlink/TelegramChatLinkMapper.java
+++ gateway/src/main/java/ru/overcode/gateway/mapper/chatlink/TelegramChatLinkMapper.java
@@ -1,10 +1,11 @@
-package ru.overcode.gateway.dto.chatlink;
+package ru.overcode.gateway.mapper.chatlink;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import ru.overcode.gateway.config.mapper.MappersConfig;
 import ru.overcode.gateway.model.chatlink.TelegramChatLink;
 
-@Mapper(componentModel = "spring")
+@Mapper(config = MappersConfig.class)
 public interface TelegramChatLinkMapper {
 
     @Mapping(target = "id", ignore = true)
